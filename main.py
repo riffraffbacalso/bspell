4cbe6dc7082c40cb668898fd22359d9d26426e56
riffraffbacalso <rbacalso1024@gmail.com>
2022-10-19 18:06:04 +0100

    Format solutions output

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import textwrap as tw
 from scrape import get_letters
 from solve import solve_bee
 
 
 def main() -> None:
-    print(solve_bee(get_letters()))
+    solutions = solve_bee(get_letters())
+    words = tw.wrap(text=" ".join(solutions))
+    print(*words, sep="\n")
 
 
 if __name__ == "__main__":
