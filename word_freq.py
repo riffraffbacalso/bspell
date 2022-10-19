eab56128316699250c7d871691dcfe6dda22376f
riffraffbacalso <rbacalso1024@gmail.com>
2022-10-19 21:44:40 +0100

    Refactor and type annotate word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -2,20 +2,23 @@ from concurrent.futures import ThreadPoolExecutor
 import httpx
 
 
-def get_ranks(words):
+def get_ranks(words: list[str]) -> dict[str, float]:
     url = "https://api.datamuse.com/words"
     with httpx.Client(http2=True) as client:
 
-        def get_rank(word):
-            return client.get(f"{url}?sp={word}&md=f&max=1").json()
+        def request(word: str) -> httpx.Response:
+            return client.get(f"{url}?sp={word}&md=f&max=1")
 
-        with ThreadPoolExecutor(max_workers=200) as pool:
-            ranks = [float(obj[0]["tags"][0][2:]) for obj in pool.map(get_rank, words)]
+        def extract(res: httpx.Response) -> float:
+            return float(res.json()[0]["tags"][0][2:])
+
+        with ThreadPoolExecutor(max_workers=50) as pool:
+            ranks = [extract(res) for res in pool.map(request, words)]
 
     return dict(zip(words, ranks))
 
 
-def sort_words(words):
+def sort_words(words) -> list[str]:
     ranks = get_ranks(words)
     return sorted(words, key=lambda x: ranks[x], reverse=True)
 
