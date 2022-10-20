b6424cf28cf26939555d63e1a3e37975e4606ef1
riffraffbacalso <rbacalso1024@gmail.com>
2022-10-20 18:10:31 +0100

    Remove NLTK corpus duplicate words
    Just use "en" dataset instead of "en" and "en-basic".
    Ensure set difference of the two is still included.
    https://github.com/nltk/nltk_data/issues/61

--- solve.py
+++ solve.py
@@ -5,11 +5,13 @@ nltk.download("words", quiet=True)
 
 from nltk.corpus import words
 
+MISSING_WORDS = ["near", "behaviour", "harbour", "humour", "box", "colour"]
+
 
 def solve_bee(letters: str) -> list[str]:
     return [
         word
-        for word in words.words()
+        for word in words.words("en") + MISSING_WORDS
         if bool(re.match(rf"[{letters}]+\Z", word))
         and len(word) >= 4
         and letters[0] in word
