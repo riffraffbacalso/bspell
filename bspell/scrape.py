3f5cef46d55d3cf960abc170482660e3b9926e4c
riffraffbacalso <rbacalso1024@gmail.com>
2022-12-07 17:10:21 +0000

    Retry puzzle requests

--- bspell/scrape.py
+++ bspell/scrape.py
@@ -3,11 +3,17 @@ import json
 from bs4 import BeautifulSoup as BS
 import httpx
 
+from retry_msg import retry_msg
+
 URL = "https://www.nytimes.com/puzzles/spelling-bee"
 
 
 def get_problem() -> str:
-    res = httpx.get(URL, follow_redirects=True)
+    @retry_msg("persistent network error when requesting today's puzzle")
+    def request_page() -> httpx.Response:
+        return httpx.get(URL, follow_redirects=True)
+
+    res = request_page()
     soup = BS(res.text, "html.parser")
     js = soup.find_all("script")[2].string
     obj_str = js[18:]
